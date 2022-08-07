5254b8d1dc23f7364b68bcb7c455beb557070cef
Franco Traverso <franco.traverso@globallogic.com>
2022-08-07 19:51:56 -0300

    Add Login test file

---
+++ src/components/Login.test.js
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/react";
+import Login from "./Login";
+
+test("username input should be rendered", () => {
+  render(<Login />);
+  const userInputEl = screen.getByPlaceholderText(/username/i);
+  expect(userInputEl).toBeInTheDocument();
+});
+
+test("password input should be rendered", () => {
+  render(<Login />);
+  const paswordInputEl = screen.getByPlaceholderText(/password/i);
+  expect(paswordInputEl).toBeInTheDocument();
+});
+
+test("button input should be rendered", () => {
+  render(<Login />);
+  const buttonInputEl = screen.getByRole("button");
+  expect(buttonInputEl).toBeInTheDocument();
+});
