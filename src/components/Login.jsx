e5c4c5eb56a1224e3ce789fbf7b36e0ba3fc8455
Franco Traverso <franco.traverso@globallogic.com>
2022-08-08 14:50:33 -0300

    Display error message only when an input is incorrect

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
+
 const Login = () => {
+  const [error, setError] = useState(false);
   return (
     <div className="container">
       <form>
-        <input type="text" placeholder="username"/>
-        <input type="password" placeholder="password"/>
+        <input type="text" placeholder="username" />
+        <input type="password" placeholder="password" />
         <button disabled="true">Login</button>
-        <span data-testid="error">Something went wrong!</span>
+        <span
+          data-testid="error"
+          style={{ visibility: error ? "visible" : "hidden" }}
+        >
+          Something went wrong!
+        </span>
       </form>
     </div>
   );
