a5ea90289c74dc38b93cfb0a61f6f0c19af17dc8
Franco Traverso <franco.traverso@globallogic.com>
2022-08-08 15:07:17 -0300

    Import fireEvent

--- src/components/Login.test.js
+++ src/components/Login.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import Login from "./Login";
 
 test("username input should be rendered", () => {
