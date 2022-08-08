166d1451e2d64dc194fc86ef419bf92efd3dd21e
Franco Traverso <franco.traverso@globallogic.com>
2022-08-08 15:15:09 -0300

    Add test that asserts if submit button is disabled when there is no input available

--- src/components/Login.test.js
+++ src/components/Login.test.js
@@ -60,3 +60,17 @@ test("error message should not be visible", () => {
   const errorEl = screen.getByTestId("error");
   expect(errorEl).not.toBeVisible();
 });
+
+test("button should not be disabled when input exists", () => {
+  render(<Login />);
+  const buttonEl = screen.getByRole("button");
+  const usernameInputEl = screen.getByPlaceholderText(/username/i);
+  const passwordInputEl = screen.getByPlaceholderText(/password/i);
+
+  const testValue = "test";
+
+  fireEvent.change(usernameInputEl, { target: { value: testValue } });
+  fireEvent.change(passwordInputEl, { target: { value: testValue } });
+
+  expect(buttonEl).not.toBeDisabled();
+});
